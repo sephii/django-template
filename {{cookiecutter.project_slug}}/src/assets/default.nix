7a14d1b4c6d545730b9c352edee3cdabfdd91251
Sylvain Fankhauser <sephi@fhtagn.top>
2023-07-30 16:32:10 +0200

    Fix Tailwind classes collection

--- {{cookiecutter.project_slug}}/src/assets/default.nix
+++ {{cookiecutter.project_slug}}/src/assets/default.nix
@@ -7,9 +7,11 @@ in {
 
   static = stdenv.mkDerivation {
     name = "{{ cookiecutter.project_slug }}-assets";
-    src = ./.;
+    # It’s recessary to include the whole source so that Tailwind knows the classes in use
+    src = ./..;
     buildInputs = [ nodejs ];
     buildPhase = ''
+      cd assets
       ln -s ${nodeDependencies}/lib/node_modules ./node_modules
       export PATH="${nodeDependencies}/bin:$PATH"
       npm run build
